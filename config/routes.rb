14915554e1c0c6865aeb1a6e62ede71b198b28b3
EC2 Default User <ec2-user@ip-172-31-89-202.ec2.internal>
2021-11-02 06:08:11 +0000

    [Update] ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  resources :books
 end
